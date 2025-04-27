bcba2135339a090c2207750b51bc1ba69c2e2bde
Abdullah <abdulhaseeb9274@gmail.com>
2025-04-27 22:31:16 +0500

    initialized the project

---
+++ src/test/java/com/sprintpredictor/SprintPredictorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprintpredictor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintPredictorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
